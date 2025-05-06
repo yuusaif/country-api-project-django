854c676d92bef3557bc07394f847039c72cfeb91
yuusaif <alvi676@gmail.com>
2025-05-07 00:37:14 +0600

    New: Country model to store fetched API data

--- countries/models.py
+++ countries/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class BaseModel(models.Model):
+    created_at=models.DateTimeField(db_index=True, default=timezone.now)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract=True
+
+
+class Country(BaseModel):
+    name=models.CharField(max_length=255)
+    official_name=models.CharField(max_length=255)
+    currencies=models.CharField(max_length=100)
+    capital=models.CharField(max_length=255)
+    region=models.CharField(max_length=255)
+    subregion=models.CharField(max_length=255)
+    area=models.FloatField()
+    flag_url=models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
